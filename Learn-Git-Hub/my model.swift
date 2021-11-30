b3770450ace8e2b497fea5a338b8c4e8fe232b55
Jakobe16 <jakostom20@gmail.com>
2021-11-30 12:56:24 -0600

    hdtdfhdfhgfgc

---
+++ Learn-Git-Hub/my model.swift
@@ -0,0 +1,13 @@
+//
+//  my model.swift
+//  Learn-Git-Hub
+//
+//  Created by PM JavaScript - Ja'kobe Stumon on 11/30/21.
+//
+
+import Foundation
+
+struct MyModel {
+    var name: String?
+    
+}
