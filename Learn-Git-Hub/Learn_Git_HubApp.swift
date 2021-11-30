4316ecd324e77de23fc6a9b489162c43d8abffcd
Jakobe16 <jakostom20@gmail.com>
2021-11-30 11:47:18 -0600

    Initial Commit

---
+++ Learn-Git-Hub/Learn_Git_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_Git_HubApp.swift
+//  Learn-Git-Hub
+//
+//  Created by PM JavaScript - Ja'kobe Stumon on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Learn_Git_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
